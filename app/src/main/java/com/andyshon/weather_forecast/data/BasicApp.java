c4827c074fcfc6d92f4c3e84ff04f6b9623f14b0
Andyshon <andyshon.shkatula@gmail.com>
2018-08-04 23:43:35 +0300

    removed leakcanary dependencies

--- app/src/main/java/com/andyshon/weather_forecast/data/BasicApp.java
+++ app/src/main/java/com/andyshon/weather_forecast/data/BasicApp.java
@@ -3,7 +3,6 @@ package com.andyshon.weather_forecast.data;
 import android.app.Application;
 
 import com.andyshon.weather_forecast.data.local.DataRepository;
-import com.squareup.leakcanary.LeakCanary;
 
 /**
  * Created by andyshon on 30.07.18.
@@ -15,12 +14,6 @@ public class BasicApp extends Application {
     public void onCreate() {
         super.onCreate();
 
-        if (LeakCanary.isInAnalyzerProcess(this)) {
-            // This process is dedicated to LeakCanary for heap analysis.
-            // You should not init your app in this process.
-            return;
-        }
-        LeakCanary.install(this);
     }
 
     public AppDatabase getDatabase() {
